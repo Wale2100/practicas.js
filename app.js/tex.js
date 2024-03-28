078439a41d8d663d67f5635541ea32f0aa553946
Wilson Piña <wilsonale@hotmail.com>
2024-03-28 18:19:55 +0000

    segundo ejercicio

---
+++ app.js/tex.js
@@ -0,0 +1,6 @@
+let a, b;
+a = 2; b = 4;
+resultado = a *= b;
+document.write(`Variable a contiene ${a} <br> ` );
+document.write(`Variable b contiene ${b} <br> ` );
+document.write(`El producto de a por b es ${a,b} `);
